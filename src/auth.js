c6dbd4d021eb0c0d889ef4f662315b1415732bb5
Christian Andreas Westby <144798762+ChristianWestby@users.noreply.github.com>
2024-11-08 21:48:08 +0100

    dist install

--- src/auth.js
+++ src/auth.js
@@ -3,8 +3,6 @@ export function login() {
   localStorage.setItem('authToken', 'dummyToken');
   window.location.href = '/src/feed/index.html';
 }
-
-// Funksjon for å sjekke autentisering
 export function checkAuth() {
   const token = localStorage.getItem('authToken');
   if (!token) {
--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,10 @@
-// src/utils.js
 
-// src/utils.js
-
-// Login function
-export function login() { // Eksport direkte fra funksjonen
+export function login() {
     console.log("Login function called");
     localStorage.setItem('authToken', 'dummyToken');
     window.location.href = '/src/feed/index.html';
 }
 
-// Check authentication function
 export function checkAuth() {
     const token = localStorage.getItem('authToken');
     if (!token) {
