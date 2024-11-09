afa3999659786c6e37a6a6dd7aa7f885912cce9e
Christian Andreas Westby <144798762+ChristianWestby@users.noreply.github.com>
2024-11-09 14:34:22 +0100

    changed auth.js

--- src/auth.js
+++ src/auth.js
@@ -1,11 +1,11 @@
 
 export function login() {
   localStorage.setItem('authToken', 'dummyToken');
-  window.location.href = '/src/feed/index.html';
+  window.location.href = '../src/feed/index.html';
 }
 export function checkAuth() {
   const token = localStorage.getItem('authToken');
   if (!token) {
-    window.location.href = '/index.html';
+    window.location.href = '../index.html';
   }
 }
